aadac8e5b2e1d0c5264471f392af0fbe5e246b3a
codeworkx <daniel.hillenbrand@codeworkx.de>
2011-10-16 01:16:55 +0200

    added dsp

--- galaxysl-vendor-blobs.mk
+++ galaxysl-vendor-blobs.mk
@@ -69,6 +69,48 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/galaxysl/proprietary/graphics/lib/egl/libGLESv2_POWERVR_SGX530_125.so:system/lib/egl/libGLESv2_POWERVR_SGX530_125.so \
     vendor/samsung/galaxysl/proprietary/graphics/lib/hw/gralloc.latona.so:system/lib/hw/gralloc.latona.so
 
+# dsp
+PRODUCT_COPY_FILES += \
+    vendor/samsung/galaxysl/proprietary/dsp/720p_h264vdec_sn.dll64P:system/lib/dsp/720p_h264vdec_sn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/720p_mp4vdec_sn.dll64P:system/lib/dsp/720p_mp4vdec_sn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/720p_mp4venc_sn.dll64P:system/lib/dsp/720p_mp4venc_sn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/baseimage.dof:system/lib/dsp/baseimage.dof \
+    vendor/samsung/galaxysl/proprietary/dsp/baseimage.map:system/lib/dsp/baseimage.map \
+    vendor/samsung/galaxysl/proprietary/dsp/chromasuppress.l64p:system/lib/dsp/chromasuppress.l64p \
+    vendor/samsung/galaxysl/proprietary/dsp/conversions.dll64P:system/lib/dsp/conversions.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/dctn_dyn.dll64P:system/lib/dsp/dctn_dyn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/ddspbase_tiomap3430.dof64P:system/lib/dsp/ddspbase_tiomap3430.dof64P \
+    vendor/samsung/galaxysl/proprietary/dsp/dfgm.dll64P:system/lib/dsp/dfgm.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/dynbase_tiomap3430.dof64P:system/lib/dsp/dynbase_tiomap3430.dof64P \
+    vendor/samsung/galaxysl/proprietary/dsp/eenf_ti.l64P:system/lib/dsp/eenf_ti.l64P \
+    vendor/samsung/galaxysl/proprietary/dsp/h264vdec_sn.dll64P:system/lib/dsp/h264vdec_sn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/h264venc_sn.dll64P:system/lib/dsp/h264venc_sn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/ipp_sn.dll64P:system/lib/dsp/ipp_sn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/jpegdec_sn.dll64P:system/lib/dsp/jpegdec_sn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/jpegenc_sn.dll64P:system/lib/dsp/jpegenc_sn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/m4venc_sn.dll64P:system/lib/dsp/m4venc_sn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/monitor_tiomap3430.dof64P:system/lib/dsp/monitor_tiomap3430.dof64P \
+    vendor/samsung/galaxysl/proprietary/dsp/mp3dec_sn.dll64P:system/lib/dsp/mp3dec_sn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/mp4v720parcdec_sn.dll64P:system/lib/dsp/mp4v720parcdec_sn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/mp4varcdec_sn.dll64P:system/lib/dsp/mp4varcdec_sn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/mp4vdec_sn.dll64P:system/lib/dsp/mp4vdec_sn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/mpeg4aacdec_sn.dll64P:system/lib/dsp/mpeg4aacdec_sn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/mpeg4aacenc_sn.dll64P:system/lib/dsp/mpeg4aacenc_sn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/mpeg4aridec_sn.dll64P:system/lib/dsp/mpeg4aridec_sn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/nbamrdec_sn.dll64P:system/lib/dsp/nbamrdec_sn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/nbamrenc_sn.dll64P:system/lib/dsp/nbamrenc_sn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/postprocessor_dualout.dll64P:system/lib/dsp/postprocessor_dualout.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/qosdyn_3430.dll64P:system/lib/dsp/qosdyn_3430.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/ringio.dll64P:system/lib/dsp/ringio.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/star.l64P:system/lib/dsp/star.l64P \
+    vendor/samsung/galaxysl/proprietary/dsp/usn.dll64P:system/lib/dsp/usn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/vpp_sn.dll64P:system/lib/dsp/vpp_sn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/wbamrdec_sn.dll64P:system/lib/dsp/wbamrdec_sn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/wbamrenc_sn.dll64P:system/lib/dsp/wbamrenc_sn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/wmadec_sn.dll64P:system/lib/dsp/wmadec_sn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/wmv9dec_sn.dll64P:system/lib/dsp/wmv9dec_sn.dll64P \
+    vendor/samsung/galaxysl/proprietary/dsp/yuvconvert.l64p:system/lib/dsp/yuvconvert.l64p
+
 # keychars
 PRODUCT_COPY_FILES += \
     vendor/samsung/galaxysl/proprietary/keychars/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin \
